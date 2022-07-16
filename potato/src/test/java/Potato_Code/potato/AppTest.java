12137b678c5b32d795202fa18b6c7193f1a76e16
taikichu <107248070+taikichu@users.noreply.github.com>
2022-07-16 10:54:37 +0900

    Add files via upload

---
+++ potato/src/test/java/Potato_Code/potato/AppTest.java
@@ -0,0 +1,20 @@
+package Potato_Code.potato;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
